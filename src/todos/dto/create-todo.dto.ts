bd73ad16dfc32f402c6f390254a0eaddfa5f2602
wonjun <zeroforever@naver.com>
2025-02-11 01:12:05 +0900

    feat: class validator 설정 추가

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,5 +1,19 @@
+import { IsBoolean, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateTodoDto {
+  @IsString({
+    message: '제목은 문자열만 가능합니다. - $value',
+  })
+  @MinLength(6, {
+    message: '제목은 6자 이상 입력해주세요. - $value',
+  })
+  @MaxLength(30, {
+    message: '제목의 최대 입력 글자는 30자입니다. - $value',
+  })
   title: string;
+
+  @IsString()
   memo: string;
+  @IsBoolean()
   isDone: boolean;
 }
