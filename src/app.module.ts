bc044f34729396633d812e28c29ad9f595ecc723
wonjun <zeroforever@naver.com>
2025-02-22 17:23:15 +0900

    feat: ConfigModule 설정 추가 - NODE_ENV 값에 따른 참조할 .env 파일명 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TodosModule, AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TodosModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
