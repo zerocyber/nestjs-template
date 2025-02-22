291ab64bd39e0d81ec65608930e13b875a41be27
wonjun <zeroforever@naver.com>
2025-02-22 13:54:34 +0900

    feat: user, auth 기본 틀 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TodosModule],
+  imports: [TodosModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
