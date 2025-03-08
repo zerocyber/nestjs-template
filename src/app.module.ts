d53fda2020a834e8e4f39ac686dd9e2530674a0a
wonjun <zeroforever@naver.com>
2025-03-08 13:20:00 +0900

    feat: logging 미들웨어 기능 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerMiddleware } from './middlewares/logging.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
     }),
     TodosModule,
     AuthModule,
@@ -19,4 +20,14 @@ import { ConfigModule } from '@nestjs/config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+  configure(consumer: MiddlewareConsumer) {
+    const middlewares = [
+      LoggerMiddleware,
+    ];
+
+    // 모든 요청에 대해 미들웨어 적용
+    consumer.apply(...middlewares).forRoutes('*');
+  }
+}
---
+++ src/interceptors/logging.interceptor.ts
@@ -0,0 +1,22 @@
+
+/**
+ * 모든 요청에 대한 로깅 인터셉터 샘플
+ */
+
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('Before...');
+
+    const now = Date.now();
+    return next
+      .handle()
+      .pipe(
+        tap(() => console.log(`After... ${Date.now() - now}ms`)),
+      );
+  }
+}
