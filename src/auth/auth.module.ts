b407e1124e8bceab9c38a40df8aac2f94bac0926
zerocyber <zeroforever@naver.com>
2025-03-12 19:16:44 +0900

    feat: 인증 모듈 기본 구조 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { SessionSerializer } from './session.serializer';
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, SessionSerializer],
+  imports: [UserModule, PassportModule.register({ session: true })],
 })
 export class AuthModule {}
